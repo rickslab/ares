1d91429c16d1d026cf23bb332b08a9993c099cef
Rick.Yan <rickone2006@qq.com>
2022-03-28 10:03:41 +0800

    fix: mq publish return error

--- mq/producer.go
+++ mq/producer.go
@@ -9,7 +9,6 @@ import (
 	"github.com/nsqio/go-nsq"
 	"github.com/rickslab/ares/config"
 	"github.com/rickslab/ares/util"
-	"github.com/sirupsen/logrus"
 )
 
 var (
@@ -28,49 +27,38 @@ func getProducer() *nsq.Producer {
 	return producerCli
 }
 
-func Publish(topic string, body []byte) {
-	err := getProducer().Publish(topic, body)
-	if err != nil {
-		logrus.WithFields(logrus.Fields{
-			"topic": topic,
-		}).Error("Publish failed", err)
-	}
+func Publish(topic string, body []byte) error {
+	return getProducer().Publish(topic, body)
 }
 
-func DeferredPublish(topic string, delay time.Duration, body []byte) {
-	err := getProducer().DeferredPublish(topic, delay, body)
-	if err != nil {
-		logrus.WithFields(logrus.Fields{
-			"topic": topic,
-			"delay": delay.Seconds(),
-		}).Error("DeferredPublish failed", err)
-	}
+func DeferredPublish(topic string, delay time.Duration, body []byte) error {
+	return getProducer().DeferredPublish(topic, delay, body)
 }
 
-func PublishJSON(topic string, value interface{}) {
+func PublishJSON(topic string, value interface{}) error {
 	data, err := json.Marshal(value)
 	util.AssertError(err)
 
-	Publish(topic, data)
+	return Publish(topic, data)
 }
 
-func DeferredPublishJSON(topic string, delay time.Duration, value interface{}) {
+func DeferredPublishJSON(topic string, delay time.Duration, value interface{}) error {
 	data, err := json.Marshal(value)
 	util.AssertError(err)
 
-	DeferredPublish(topic, delay, data)
+	return DeferredPublish(topic, delay, data)
 }
 
-func PublishProto(topic string, value proto.Message) {
+func PublishProto(topic string, value proto.Message) error {
 	data, err := proto.Marshal(value)
 	util.AssertError(err)
 
-	Publish(topic, data)
+	return Publish(topic, data)
 }
 
-func DeferredPublishProto(topic string, delay time.Duration, value proto.Message) {
+func DeferredPublishProto(topic string, delay time.Duration, value proto.Message) error {
 	data, err := proto.Marshal(value)
 	util.AssertError(err)
 
-	DeferredPublish(topic, delay, data)
+	return DeferredPublish(topic, delay, data)
 }
