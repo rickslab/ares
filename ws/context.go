af017d6f652baca0780339739bf5c544a455d240
Rick.Yan <rickone2006@qq.com>
2023-11-24 12:57:34 +0800

    fix: ws remove user id

--- ws/context.go
+++ ws/context.go
@@ -58,7 +58,6 @@ func NewContext(r *http.Request, conn *websocket.Conn, service string, method st
 		"scope", scope,
 		"caller", service,
 	}
-	c.values["UserId"] = userId
 
 	go c.writeLoop()
 	return c
